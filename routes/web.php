b14e0f136031d93826aa06309ffb07031a3f8391
Thiago Castro <thiagocastro.as@gmail.com>
2023-09-12 18:38:47 -0300

    Rotas da classe Fornecedor

--- routes/web.php
+++ routes/web.php
@@ -20,8 +20,24 @@ Route::get('/', function () {
 
 Route::prefix('produtos')->group(function () {
     Route::get('/', [ProdutosController::class, 'index'])->name('produto.index');
+    // Criar Cadastro
     Route::get('/cadastrarProduto', [ProdutosController::class, 'cadastrarProduto'])->name('cadastrar.produto');
-    Route::post('/cadastrarProduto', [ProdutosController::class, 'cadastrarProduto'])->name('cadastrar.produto');
-    
+    Route::post('/cadastrarProduto', [ProdutosController::class, 'cadastrarProduto'])->name('cadastrar.produto');  
+    // Atualizar Cadastro
+    Route::get('/atualizarProduto/{id}', [ProdutosController::class, 'atualizarProduto'])->name('atualizar.produto');
+    Route::put('/atualizarProduto/{id}', [ProdutosController::class, 'atualizarProduto'])->name('atualizar.produto');  
+    //Deletar Cadastro
     Route::delete('/delete', [ProdutosController::class, 'delete'])->name('produto.delete');
+});
+
+Route::prefix('fornecedores')->group(function () {
+    Route::get('/', [fornecedoresController::class, 'index'])->name('fornecedores.index');
+    // Criar Cadastro
+    Route::get('/cadastrarFornecedor', [FornecedoresController::class, 'cadastrarFornecedores'])->name('cadastrar.fornecedor');
+    Route::post('/cadastrarFornecedor', [FornecedoresController::class, 'cadastrarCliente'])->name('cadastrar.fornecedor');  
+    // Atualizar Cadastro
+    Route::get('/atualizarFornecedor/{id}', [FornecedoresController::class, 'atualizarCliente'])->name('atualizar.fornecedor');
+    Route::put('/atualizarFornecedor/{id}', [FornecedoresController::class, 'atualizarCliente'])->name('atualizar.fornecedor');  
+    //Deletar Cadastro
+    Route::delete('/delete', [FornecedoresController::class, 'delete'])->name('fornecedor.delete');
 });
